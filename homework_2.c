30855b174fa9bc919aaff25b64e139ca7b296fba
farydcortes <78566552+farydcortes@users.noreply.github.com>
2021-03-04 03:17:18 -0600

    Add files via upload

---
+++ homework_2.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+
+int k;
+printf ("introduzca la cantidad de terminos primos que desea saber: ");
+scanf("%i", &k);
+
+  int count, i, flag, sr;
+  int  b, l, a[1000];
+  int n=1;
+  count = 1;
+  	l=0;
+  	
+  while(count <= k){
+    sr = sqrt(n); 
+    flag = 0; //Tengo como si ya tuviera un numero primo
+    for(i=2; i<= sr; i++){
+      if(n%i==0){
+        flag = 1; //Si no es primo flag = 1 y no se cumple el siguiente if
+        break;
+      }
+   
+    }
+
+    if(flag==0){
+      printf("%d \n", n);
+      	l=0;
+ 	while (n > 0) {
+ 		a[l]= n % 2;
+ 		n /= 2;
+ 		l++;
+	 }
+ 	for(b = l- 1; b >= 0; b--){
+ 		printf("%d", a[b]);
+ 		
+	 }
+      count++; //Conteo de primos
+    }
+    
+    n++; //Siguiente numero
+    
+  } 
+}
+

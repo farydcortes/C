3efe0a8a09180cd58f434552e58a161e5fe83c1c
farydcortes <78566552+farydcortes@users.noreply.github.com>
2021-03-04 03:22:49 -0600

    Update homework_2.c

--- homework_2.c
+++ homework_2.c
@@ -6,16 +6,14 @@ int k;
 printf ("introduzca la cantidad de terminos primos que desea saber: ");
 scanf("%i", &k);
 
-  int count, i, flag, sr;
-  int  b, l, a[1000];
+  int count, i, flag, sqr;
   int n=1;
   count = 1;
-  	l=0;
   	
   while(count <= k){
-    sr = sqrt(n); 
+    sqr = sqrt(n); 
     flag = 0; 
-    for(i=2; i<= sr; i++){
+    for(i=2; i<= sqr; i++){
       if(n%i==0){
         flag = 1; 
         break;
@@ -24,17 +22,7 @@ scanf("%i", &k);
     }
 
     if(flag==0){
-      printf("%d \n", n);
-      	l=0;
- 	while (n > 0) {
- 		a[l]= n % 2;
- 		n /= 2;
- 		l++;
-	 }
- 	for(b = l- 1; b >= 0; b--){
- 		printf("%d", a[b]);
- 		
-	 }
+      printf("%d \n", n);      
       count++;
     }
     
